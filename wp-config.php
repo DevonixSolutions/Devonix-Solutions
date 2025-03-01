5db981feb510095e1cb9ceb77f7f4cdbd768c7e0
DevonixSolutions <devonixsolutions@gmail.com>
2025-03-01 19:43:39 +0500

    Devonix Site being ready

---
+++ wp-config.php
@@ -0,0 +1,103 @@
+<?php
+define( 'WP_CACHE', true );
+
+
+
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the website, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
+ *
+ * @package WordPress
+ */
+
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'devonixsolutions_db' );
+
+/** Database username */
+define( 'DB_USER', 'root' );
+
+/** Database password */
+define( 'DB_PASSWORD', '' );
+
+/** Database hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '>F~w=!D.py;.m5X]?EA}snxjg=w3;F+FRZa]LR0XVm)A;FYn!%_N]Mzs[WI8]X$p' );
+define( 'SECURE_AUTH_KEY',  'd6g%%SpnQZ3^w0#JP@.,X6n_P NPPn6`QWP[tC51!-%ia)(Fxv=Y(.x0(W>> |:B' );
+define( 'LOGGED_IN_KEY',    '+9MdR-ynyt>PwGw| edM!XPdT9(J#<J;b|[vDN,0B/R;(;DRj&{_GR~JvlTVvLzK' );
+define( 'NONCE_KEY',        ',}y1[|,*>~:ZPQ;tQ}3ndQ9g,l@wQHKtk^J73@]#}xZy%0t1a!<YUbZL3 ;J|D+[' );
+define( 'AUTH_SALT',        'P3o~>|@w!WV$^D7Fo1_]=IjRp3y|Df#R_GyR4j7phFTy5;uOnL.S?6X,J~t=l{ex' );
+define( 'SECURE_AUTH_SALT', ']0YN?rGkbo7DLkYP*u.b^idp6Fh4ki95_fD.I;~ydny3r3a^Ql{:4J*gqi3e4KB*' );
+define( 'LOGGED_IN_SALT',   'r7-7jJhz(>R5Ln$!DDI9)6gGgSS+f/pCY@*X9wnkb{=7bp(GzU|<A+ru?Vb6{[kJ' );
+define( 'NONCE_SALT',       'qqX}(XcRB;<koS?/b2n1w8+fBfS qC(w2&Y9& xyF%f4/JGw@33S`UK_Hm-&-Xcv' );
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
+// Add these two lines to define the Home and Site URLs
+define('WP_HOME', 'http://localhost/myPersonalSites/Devonix-Solutions');
+define('WP_SITEURL', 'http://localhost/myPersonalSites/Devonix-Solutions');
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
